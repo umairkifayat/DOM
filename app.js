b53c32d529efcee79c7ff184661bbdae21a6b90e
umair kifayat <umairkifayat3@gmail.com>
2023-08-16 21:51:18 -0700

    DOM

---
+++ app.js
@@ -0,0 +1,77 @@
+// const h1 = document.getElementById("head-1")
+// console.log(h1);
+// // const h1 = document.querySelector("#head-1")
+// // console.log(h1.innerHTML);
+// h1.innerHTML = "UMair";
+
+
+
+
+
+
+
+
+
+
+
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const login= document.querySelector("#login")
+
+
+console.log(email);
+console.log(password);
+console.log(login);
+
+
+function loginuser(){
+    console.log("hello user");
+    console.log(email.value);
+    console.log(password.value);
+    // alert("alert clicked")
+    
+}
+
+
+// const email = document.querySelector("#email");
+// const password = document.querySelector("#password");
+// const login = document.querySelector("#login");
+
+// console.log(email);
+// console.log(password);
+// console.log(login);
+
+// function loginuser() {
+//     switch (true) {
+//         case loginuser:
+//             console.log("phla case chl gy bahi");
+//             break;
+//     }
+//     console.log("hello user");
+//     console.log(email.value);
+//     console.log(password.value);
+//     alert();
+// }
+
+// const emailInput = document.querySelector("#email");
+// const passwordInput = document.querySelector("#password");
+// const loginButton = document.querySelector("#login");
+
+// loginButton.addEventListener("click",alert, loginuser);
+
+// function loginuser() {
+//     console.log("clicked login");
+//     const emailValue = emailInput.value;
+//     const passwordValue = passwordInput.value;
+
+//     // Perform actual login-related tasks here, e.g., send data to a server for authentication
+
+//     console.log("User email:", emailValue);
+//     console.log("User password:", passwordValue);
+
+ 
+    alert("Login successful!");
+// }
+
+
+
